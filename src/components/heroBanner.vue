626c2fc79db16c24e77c62244087e650baa28b95
izrulfizal <izrulfizal1410@gmail.com>
2022-11-12 19:55:55 +0800

    Tweaked

--- src/components/heroBanner.vue
+++ src/components/heroBanner.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
-        <b-card img-src=./images/izrul.png img-left class="heroCont w-50 mx-auto" style="margin-top: 30px; padding: 30px; padding-bottom: 0px">
-<h1>Izrulfizal Saufihamizal</h1>
+        <b-card img-src=./images/izrul.png img-left class="heroCont mx-auto" style="margin-top: 10%; padding: 30px; padding-bottom: 0px">
+          <b-card-text class="mt-5">
+            <h1 class="my-5">Izrulfizal Saufihamizal</h1>
+            <hr>
+            <Transition mode="out-in">
+              <h3 class="my-5" ref="positionPara" :key="position">{{ position }}</h3>
+            </Transition>
+          </b-card-text>  
         </b-card>
     </div>
   </template>
@@ -9,6 +15,30 @@
   <script>
   export default {
     name: 'heroBanner',
+    data(){
+      return{
+        options: ["Software Engineer", "Code Enthusiast", "Speaker", "Occasional Perfectionist"],
+        position: "Software Engineer",
+      }
+    },
+    async mounted(){
+      const timer = ms => new Promise(res => setTimeout(res, ms))
+
+      for (var i = 0; i < this.options.length; i++) {
+        this.position = this.options[i]
+        await timer(1500)
+        if(i === this.options.length - 1) {
+          i=-1 
+          continue
+        }
+      }
+    },
+    methods:{
+      changePosition(i){
+        setTimeout(() => { console.log(this.options[i]) }, 1000);
+      },  
+    }
+
   }
   </script>
   
@@ -32,4 +62,15 @@
     background-color: #B8BEE3;
   }
   </style>
+  <style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
   
